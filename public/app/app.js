8b517c34d65807630001073b98327664c075f778
Álvaro Fernando <alvarofernandoms@gmail.com>
2018-04-19 08:54:33 -0300

    Using takeUntil operator for calling the service only three times

--- public/app/app.js
+++ public/app/app.js
@@ -1,11 +1,15 @@
 import { log } from './utils/promises-helepers.js'
 import './utils/array-helpers.js'
 import { notasService as service } from './nota/service.js'
+import { takeUntil } from './utils/operators.js'
 
-document
-  .querySelector('#myButton')
-  .onclick = () => 
+const operation = takeUtil(3, () => {
   service
   .sumItems('2143')
   .then(console.log)
   .catch(console.log)
+})
+
+document
+  .querySelector('#myButton')
+  .onclick = operation
